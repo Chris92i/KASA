8c936cd8a413cb6d501103ce726c41313fb587e0
Chris92i <chris.p.teixeira@gmail.com>
2023-11-30 21:46:12 +0100

    dans le header rendre cliquable l'icone logo kasa

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
 import './style.scss'
 import Nav from '../Navigation/Nav'
 import logo from '../../assets/LOGO-kasa-header.svg'
+import { Link } from 'react-router-dom'
 
 function Header(){
     return (
         <header>
             <div className='header_container'>
-                <div className='logoHeader'> <img src={logo}  alt='logo kasa'/> </div>
+                <div className='logoHeader'> 
+                    <Link to='/' >
+                        <img src={logo}  alt='logo kasa'/> 
+                    </Link>
+                </div>
                 <Nav className='navigation'/>
             </div>
         </header>
