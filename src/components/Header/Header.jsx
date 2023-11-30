4fda08929b48c8ebd071fd1dc214e7757b85aec6
Chris92i <chris.p.teixeira@gmail.com>
2023-11-30 21:16:48 +0100

    correction bug logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import './style.scss'
 import Nav from '../Navigation/Nav'
+import logo from '../../assets/LOGO-kasa-header.svg'
 
 function Header(){
     return (
         <header>
             <div className='header_container'>
-                <div className='logoHeader'> <img src="../../assets/LOGO-kasa-header.svg" alt="Logo agence Kasa" /> </div>
+                <div className='logoHeader'> <img src={logo}  alt='logo kasa'/> </div>
                 <Nav className='navigation'/>
             </div>
         </header>
