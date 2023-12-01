a864433c207936b35c2c9476e6de41fec4f9eba1
Chris92i <chris.p.teixeira@gmail.com>
2023-12-01 12:31:23 +0100

    modification de la methode pour affichage du logo dans le footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,10 @@
 import './style.scss'
+import Logo from '../../assets/LOGO_footer_last.png'
 
 function Footer(){
     return (
         <footer className="footer">
-            <img src="/src/assets/LOGO_footer.svg" alt="logo_kasa" />
+            <img src={Logo} alt="logo_kasa" />
             <p className="footer_text">© 2020 Kasa. All rights reserved</p>
         </footer>
         
